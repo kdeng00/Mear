7fc074331632c84047801a25e8b594ff48c2fa4d
amazing-username <kundeng94@gmail.com>
2019-05-24 15:22:13 -0400

    Created Song Model and added the Newtonsoft.Json library #55

---
+++ Mear/Mear/Models/Song.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Newtonsoft.Json;
+
+namespace Mear.Models
+{
+    public class Song
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+        [JsonProperty("title")]
+        public string Title { get; set; }
+        [JsonProperty("album")]
+        public string Album { get; set; }
+        [JsonProperty("artist")]
+        public string Artist { get; set; }
+        [JsonProperty("year")]
+        public int Year { get; set; }
+        [JsonProperty("genre")]
+        public string Genre { get; set; }
+        [JsonProperty("duration")]
+        public int Duration { get; set; }
+        [JsonProperty("filename")]
+        public string Filename { get; set; }
+        [JsonProperty("song_path")]
+        public string SongPath { set; get; }
+    }
+}
